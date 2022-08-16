2af2bc1a3c41c5c43b0e5275bd032b8c7e3994af
Evgeny Korolev <mts9832899777@gmail.com>
2022-08-17 01:21:09 +0700

    added new branch

--- src/Components/Promo/index.js
+++ src/Components/Promo/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import styles from './Promo.module.css';
+import styles from "./Promo.module.css";
 
 const Promo = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}>MA BRAND</div>
-      <div className={styles.description}><h1>коммуникационное агентство полного цикла</h1></div>
+      <div className={styles.description}>
+        <h1>коммуникационное агентство полного цикла</h1>
+      </div>
       <div className={styles.slogan}>диалог бизнеса с клиентом под ключ</div>
     </div>
   );
-}
+};
 
 export default Promo;
